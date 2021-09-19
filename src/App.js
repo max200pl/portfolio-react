6fd2aa003155b53959c9ae4be0b0af10887fdc6a
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-09-19 14:06:24 +0200

    added routing in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
 import Intro from './components/Intro/Intro';
+import { Route, Switch } from "react-router-dom";
 
 function App()
 {
   return (
+
     <div className="App">
       <Header />
-      <Intro />
+      <Switch>
+        <Route path="/"><Intro /></Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import s from "./Header.module.scss";
 import HelloImg from "../../img/header/hello.svg";
 import PortfolioImg from "../../img/header/portfolio.svg";
@@ -11,35 +13,31 @@ const Header = (props) => {
     <header className={s.header}>
       <div id="header__container" className={s.container}>
         <nav className={s.nav} id="nav">
-          <a className={s.nav__link} href="#hello">
+          <Link className={s.nav__link} to="/">
             <img className={s.nav__img} src={HelloImg} alt="scrollup"></img>
             <span>Hello</span>
-          </a>
-          <a className={s.nav__link} href="#portfolio">
+          </Link>
+          <Link className={s.nav__link} to="/portfolio">
             <img
               className={s.nav__img}
               src={PortfolioImg}
-              alt="scroll to portfolio"
+              alt="my works portfolio"
             ></img>
             <span>Portfolio</span>
-          </a>
-          <a className={s.nav__link} href="#about">
-            <img
-              className={s.nav__img}
-              src={AboutImg}
-              alt="scroll to About me "
-            ></img>
+          </Link>
+          <Link className={s.nav__link} to="/about">
+            <img className={s.nav__img} src={AboutImg} alt="about me "></img>
             <span>About me</span>
-          </a>
-          <a
+          </Link>
+          <Link
             className={s.nav__link}
             rel="noreferrer"
-            href="https://github.com/max200pl"
+            to="https://github.com/max200pl"
             target="_blank"
           >
             <img className={s.nav__img} src={GithubImg} alt="link github"></img>
             <span>My github</span>
-          </a>
+          </Link>
           <button
             className={s.nav__link + " " + s.nav__link_btn}
             data-modal="#modal_hire-me"
