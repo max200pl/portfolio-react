56df1d085aa19ed0e40adf29b720d7d248ab869f
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2022-06-01 22:54:25 +0300

    fixed path img

--- src/components/Portfolio/Work/Work.jsx
+++ src/components/Portfolio/Work/Work.jsx
@@ -3,19 +3,19 @@ import React from "react";
 import s from "./Work.module.scss";
 
 const Work = (props) => {
-  let work = props.workItem;
-  return (
-    <div className={s.work} onClick={() => props.openModal(work)}>
-      <img className={s.work__img} src={props.imgWork} alt="work" />
-      <div className={s.work__content}>
-        <div className={s.work__cat}>category: {props.categoryWork}</div>
-        <div className={s.work__title}>
-          {props.workName}
-          <time className={s.work__date}>{props.endWorkTime}</time>
-        </div>
-      </div>
-    </div>
-  );
+	let work = props.workItem;
+	return (
+		<div className={s.work} onClick={() => props.openModal(work)}>
+			<img className={s.work__img} src={props.imgWork} alt="work" />
+			<div className={s.work__content}>
+				<div className={s.work__cat}>category: {props.categoryWork}</div>
+				<div className={s.work__title}>
+					{props.workName}
+					<time className={s.work__date}>{props.endWorkTime}</time>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Work;
