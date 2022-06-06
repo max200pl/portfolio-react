cc3654ff70c5458b96894275f46e825ed220b3ff
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2022-06-07 01:25:30 +0300

    refactoring code and added style active link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,36 +8,51 @@ import AboutImg from "../../images/header/about.svg";
 import GithubImg from "../../images/header/github.svg";
 import ContactImg from "../../images/header/contact.svg";
 
+let dataNavLink = [
+	{
+		name: "Hello",
+		to: "/",
+		alt: "base page",
+		iconUrl: HelloImg,
+	},
+	{
+		name: "Portfolio",
+		to: "/portfolio",
+		alt: "my works portfolio",
+		iconUrl: PortfolioImg,
+	},
+	{
+		name: "About me",
+		to: "/about",
+		alt: "about me",
+		iconUrl: AboutImg,
+	}
+]
+
+const NavLinkComponent = dataNavLink.map((data) =>
+	<NavLink
+		className={({ isActive }) =>
+			isActive ? s.nav__link_active : s.nav__link
+		}
+		to={data.to}
+		target={data.target}
+		rel={data.rel}
+	>
+		<img className={s.nav__img} src={data.iconUrl} alt={data.alt}></img>
+		<span>{data.name}</span>
+	</NavLink>
+)
+
 const Header = (props) => (
 	<div className="container">
 		<header className={s.header}>
 			<div id="header__container" className={s.container}>
 				<nav className={s.nav} id="nav">
-					<NavLink className={s.nav__link} to="/">
-						<img className={s.nav__img} src={HelloImg} alt="scrollup"></img>
-						<span>Hello</span>
-					</NavLink>
-					<NavLink className={s.nav__link} to="/portfolio">
-						<img
-							className={s.nav__img}
-							src={PortfolioImg}
-							alt="my works portfolio"
-						></img>
-						<span>Portfolio</span>
-					</NavLink>
-					<NavLink className={s.nav__link} to="/about">
-						<img className={s.nav__img} src={AboutImg} alt="about me "></img>
-						<span>About me</span>
-					</NavLink>
-					<NavLink
-						className={s.nav__link}
-						rel="noreferrer"
-						to={{ pathname: "https://github.com/max200pl" }}
-						target="_blank"
-					>
-						<img className={s.nav__img} src={GithubImg} alt="link github"></img>
+					{NavLinkComponent}
+					<a className={s.nav__link} href="https://github.com/max200pl" target="_blank" rel="noreferrer">
+						<img className={s.nav__img} src={GithubImg} alt="link github" />
 						<span>My github</span>
-					</NavLink>
+					</a>
 					<button className={s.nav__link + " " + s.nav__link_btn}>
 						<img className={s.nav__img} src={ContactImg} alt="contact me"></img>
 						<span>Contact me</span>
