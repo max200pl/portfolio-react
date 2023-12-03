171febfb2e4a9d237810870b32e34c08260b8ceb
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2023-12-03 12:29:45 +0200

    Loader -> created component

---
+++ client/src/components/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+
+
+import LoaderImg from "../../images/loader.svg";
+import "./Loader.scss"
+
+
+const Loader = ({
+    loading,
+}) => {
+    return (
+        <div className="loader" loading={loading} >
+            <img className="loader__img" src={LoaderImg} alt="loader circle" />
+            <span className="loader__text">Loading...</span>
+        </div>
+    )
+}
+
+export default Loader;
