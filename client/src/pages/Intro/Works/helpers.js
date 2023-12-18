20de92f18a174079b2a3ba361ce5987139b31a45
max200pl <max2000pl@gmail.com>
2023-12-18 20:06:49 +0200

    Work -> updated constructor

--- client/src/pages/Intro/Works/helpers.js
+++ client/src/pages/Intro/Works/helpers.js
@@ -1,53 +1,27 @@
 export class Work {
-    static id = ""
-    static category = ""
-    static client = ""
-    static date = undefined // UTF8 string
-    static name = ""
-    // order = "" // generate form server
-    static technology = [
-        { apply: 0, name: "" }
-    ]
-    static cardImage = {
-        name: "", // intro.svg
-        // blurHash: ""
-    }
-    static images = [
-        {
-            name: "",
-            // blurHash: ""  generate form server
-        },
-    ]
-
     constructor(
-        id,
-        category,
-        client,
-        date,
-        name,
-        technology,
-        cardImage,
-        images,
+        id = "",
+        name = "",
+        link = "",
+        category = "",
+        client = "",
+        date = undefined,
+        technology = [{ apply: 100, name: "" }],
+        cardImage = { name: "", blurHash: "" },
+        images = [{ name: "", blurHash: "" }]
     ) {
-        this.id = id
-        this.category = category
-        this.client = client
-        this.date = date
-        this.name = name
-        this.technology = technology
-        this.cardImage = cardImage
-        this.images = images
+        this.id = id;
+        this.name = name;
+        this.link = link;
+        this.category = category;
+        this.client = client;
+        this.date = date;
+        this.technology = technology;
+        this.cardImage = cardImage;
+        this.images = images;
     }
 
     static create() {
-        return {
-            category: this.category,
-            client: this.client,
-            date: this.date,
-            name: this.name,
-            technology: this.technology,
-            cardImage: this.cardImage,
-            images: this.images,
-        }
+        return new Work();
     }
 }
