7127fb8fad989cacb354138226fee25b67c4a7b2
max200pl <max2000pl@gmail.com>
2023-12-20 10:59:18 +0200

    added custom scroll bar

--- client/src/modals/ModalWorkManager/ModalWorkManagerForm/helpers.ts
+++ client/src/modals/ModalWorkManager/ModalWorkManagerForm/helpers.ts
@@ -1,41 +1,7 @@
-import { IActionButton } from "../../../assets/components/ActionButtons/ActionButtons";
 import { CheckboxesTagsOptions } from "../../../assets/components/AutocompleteTagsCheckboxesMUI/AutocompleteTagsCheckboxesMUI";
 import { InterfaceTech, InterfaceTechWithApply } from "../../../assets/interfaces/interfaces";
 import { IFormInput } from "./ModalWorkManagerForm";
 
-export const actionButtonsForm = (
-    onClose: () => {},
-): IActionButton[] => [
-        {
-            role: "secondary",
-            name: "Close",
-            type: "button",
-            onClick: () => onClose(),
-            styling: {
-                type: "advanced",
-            },
-        },
-        {
-            role: "primary",
-            name: "Save",
-            type: "submit",
-            styling: {
-                type: "advanced",
-            },
-        },
-    ];
-
-export const deleteButtonForm = (onDelete: () => {}): IActionButton[] => [
-    {
-        onClick: () => onDelete(),
-        type: "button",
-        styling: {
-            type: "advanced",
-            color: "red",
-        },
-        name: "Delete",
-    },
-];
 
 export const getOptionsGroupAutocomplete = <T extends keyof InterfaceTech>(
     technologies: InterfaceTech[T]
